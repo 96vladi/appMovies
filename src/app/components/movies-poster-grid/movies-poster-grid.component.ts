f431635f175dc90221eb59498efba29817d8fda4
Vladimir Maidana <maidana.vladi@gmail.com>
2022-10-20 17:01:21 -0400

    add: moviesPosterGrid components

---
+++ src/app/components/movies-poster-grid/movies-poster-grid.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from 'src/app/interfaces/bilboardResponse';
+
+@Component({
+  selector: 'app-movies-poster-grid',
+  templateUrl: './movies-poster-grid.component.html',
+  styleUrls: ['./movies-poster-grid.component.css']
+})
+export class MoviesPosterGridComponent implements OnInit {
+
+  @Input() movies!: Movie[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.movies)
+  }
+
+}
